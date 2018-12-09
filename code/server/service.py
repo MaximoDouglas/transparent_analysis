f68e6d4ba7e25b0fc39ce79971dbecb2d3640c95
MaximoDouglas <douglas.h.maximo@gmail.com>
2018-12-09 03:39:08 -0300

    Server basic structure created

---
+++ code/server/service.py
@@ -0,0 +1,4 @@
+from flask_cors import CORS
+from flask import Flask
+from flask_restful import Api
+from facade.facade import Facade as fc
---
+++ code/server/start.sh
@@ -0,0 +1,26 @@
+#Install python3, comment if you already have it
+#sudo apt-get install python3-pip;
+
+#Install pip, comment if you already have it
+#sudo apt-get install python3-pip;
+
+#install data mining package
+#pip3 install orange3
+
+#Install Flask, comment if you already have it
+#pip3 install Flask;
+
+#Install Flask-RESTful, comment if you already have it
+#pip3 install Flask-RESTful;
+
+#Install flask_cors, comment if you already have it
+#pip3 install -U flask-cors
+
+#Export the application to the server
+export FLASK_APP=service.py;
+
+#Defines that the actual environment are in production (this will deactivate the debugger mode)
+export FLASK_ENV=production;
+
+#Runs the server
+flask run;
