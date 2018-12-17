598eaec01cb5b7d7507e71c503eb6c2aab424809
MaximoDouglas <douglas.h.maximo@gmail.com>
2018-12-16 23:28:54 -0300

    Charts bug fixed

--- code/client/src/app/data-detail/data-detail.component.ts
+++ code/client/src/app/data-detail/data-detail.component.ts
@@ -6,6 +6,8 @@ import { Chart } from 'chart.js'
 import { Data }         from '../data';
 import { DataService }  from '../data.service';
 
+type Aux = { date: any, value: any }
+
 @Component({
   selector: 'app-data-detail',
   templateUrl: './data-detail.component.html',
@@ -33,82 +35,86 @@ export class DataDetailComponent implements OnInit {
         this.id = id;
 
         this.dataService.getData(id)
-          .subscribe(data => {
-              this.data = data
+          .subscribe( (data: Data) => {
+              this.data = data;
 
-              let value  = data['list'].map(res => res.value)
-              let dates  = data['list'].map(res => res.date)
+              if (data != null){
+                  let value  = data['list'].map((res: Aux) => res.value);
+                  let dates  = data['list'].map((res: Aux) => res.date);
 
-              this.chart = new Chart('canvas', {
-                  type: 'line',
-                  data: {
-                      labels: dates,
-                      datasets: [
-                          {
-                              data: value,
-                              borderColor: '#3cba9f',
-                              fill: false
+                  this.chart = new Chart('canvas', {
+                      type: 'line',
+                      data: {
+                          labels: dates,
+                          datasets: [
+                              {
+                                  data: value,
+                                  borderColor: '#3cba9f',
+                                  fill: false
+                              }
+                          ]
+                      },
+                      options: {
+                          layout: {
+                            padding: {
+                                left: 25,
+                                right: 25,
+                                top: 25,
+                                bottom: 25
+                            }
+                          },
+                          legend: {
+                              display: false
+                          },
+                          scales: {
+                              xAxes: [{
+                                  display: true
+                              }],
+                              yAxes: [{
+                                  display: true
+                              }]
                           }
-                      ]
-                  },
-                  options: {
-                      layout: {
-                        padding: {
-                            left: 25,
-                            right: 25,
-                            top: 25,
-                            bottom: 25
-                        }
-                      },
-                      legend: {
-                          display: false
-                      },
-                      scales: {
-                          xAxes: [{
-                              display: true
-                          }],
-                          yAxes: [{
-                              display: true
-                          }]
                       }
-                  }
-              })
+                  });
 
-              this.barChart = new Chart('barCanvas', {
-                  type: 'bar',
-                  data: {
-                      labels: dates,
-                      datasets: [
-                          {
-                              data: value,
-                              borderColor: '#3cba9f',
-                              fill: false
+                  this.barChart = new Chart('barCanvas', {
+                      type: 'bar',
+                      data: {
+                          labels: dates,
+                          datasets: [
+                              {
+                                  data: value,
+                                  borderColor: '#3cba9f',
+                                  fill: false
+                              }
+                          ]
+                      },
+                      options: {
+                          layout: {
+                            padding: {
+                                left: 25,
+                                right: 25,
+                                top: 25,
+                                bottom: 25
+                            }
+                          },
+                          legend: {
+                              display: false
+                          },
+                          scales: {
+                              xAxes: [{
+                                  display: true
+                              }],
+                              yAxes: [{
+                                  display: true
+                              }]
                           }
-                      ]
-                  },
-                  options: {
-                      layout: {
-                        padding: {
-                            left: 25,
-                            right: 25,
-                            top: 25,
-                            bottom: 25
-                        }
-                      },
-                      legend: {
-                          display: false
-                      },
-                      scales: {
-                          xAxes: [{
-                              display: true
-                          }],
-                          yAxes: [{
-                              display: true
-                          }]
                       }
-                  }
-              })
+                  });
+              }
           });
+
+
     }
 
     goBack(): void {
